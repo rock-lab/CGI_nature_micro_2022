5a2d530b8373ee0855179e7bb936e0f0a1aa45b6
zackazadian <52663836+zackazadian@users.noreply.github.com>
2022-03-30 17:01:13 -0400

    Fixing Paths
    More fixing read/output paths.

--- Gen_Weizhen_Overlap_Supplemental_Fig2.py
+++ Gen_Weizhen_Overlap_Supplemental_Fig2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import crispr_tools
 import ast
 
-WData = pd.read_csv('Weiz_wLFC.csv')
+WData = pd.read_csv('data/Weiz_wLFC.csv')
 WData['Gene_name'] = WData['Gene_name'].replace('rv','RVBD',regex=True)
 WData['Full_ID'] = WData[['Gene', 'Gene_name']].agg(':'.join, axis=1)
 
@@ -40,7 +40,7 @@ def InBoth(first, second):
     return overlap_genes
 
 # import Day1+Day5 data
-dat_path = '/Users/zacharyazadian/RockLab Dropbox/Zachary Azadian/ChemGen/New_Data_Results/AAA_Final_Data/'
+dat_path = 'data/'
 ourData = pd.read_csv(dat_path+'Final_PreDepletion_Grouped_Split.csv', index_col=0)
 ourSmall = ourData[ourData.Comparison.isin(['INH_5', 'Emb_1', 'Rif_25','Vanc_25'])]
 ourSmall['Essential Depletion Day1+Day5 List'] = ourSmall['Essential Depletion Day1+Day5 List'].map(ast.literal_eval)
@@ -123,7 +123,7 @@ Vanc_Tab = gen_overlap_column(W_Vanc_Tab, overlap_list_vanc)
 INH_Tab = gen_overlap_column(W_INH_Tab, overlap_list_inh)
 Emb_Tab = gen_overlap_column(W_Emb_Tab, overlap_list_emb)
 
-output_path = '/Users/zacharyazadian/RockLab Dropbox/Zachary Azadian/ChemGen/New_Data_Results/AAA_Final_Data/Weizhen_Comp/Supplement/'
+output_path = 'Results/Supplement_Data_2/'
 # Rif_Tab.to_csv(output_path+'Rif.csv')
 # Vanc_Tab.to_csv(output_path+'Vanc.csv')
 # INH_Tab.to_csv(output_path+'INH.csv')
@@ -157,9 +157,7 @@ vanc_file5 = 'result_1975_VAN_0_25X_D5_vs_1972_DMSO_D5_alphamedian_control_contr
 def gen_Ci_table(gene_list, treatment_file, prefix):
     AllDrug_Table = pd.DataFrame(data=gene_list, columns = ['Gene_ID'])
 
-    m_path = '/Users/zacharyazadian/RockLab Dropbox/Projects/chemical_genomics/data/MAGECK/'+treatment_file
-    # M = crispr_tools.data.results.open_file(m_path)
-    # M_df = M.data
+    m_path = 'data/mageck/'+treatment_file
     M_df = pd.read_csv(m_path, sep='\t')
 
     l2fc_list = []
@@ -187,19 +185,19 @@ def gen_Ci_table(gene_list, treatment_file, prefix):
 
 ## written to output intermediate tables (to check) and then load back in below ##
 emb_Ci = gen_Ci_table(emb_gene_list, emb_file, prefix='D1_')
-emb_Ci.to_csv('Supplement/Emb_Ci.csv')
+#emb_Ci.to_csv('Supplement/Emb_Ci.csv')
 emb_Ci_5 = gen_Ci_table(emb_gene_list, emb_file5, prefix='D5_')
-emb_Ci_5.to_csv('Supplement/Emb_Ci_5.csv')
+#emb_Ci_5.to_csv('Supplement/Emb_Ci_5.csv')
 
 inh_Ci = gen_Ci_table(inh_gene_list, inh_file, prefix = 'D1_')
-inh_Ci.to_csv('Supplement/INH_Ci.csv')
+#inh_Ci.to_csv('Supplement/INH_Ci.csv')
 inh_Ci_5 = gen_Ci_table(inh_gene_list, inh_file5, prefix = 'D5_')
-inh_Ci_5.to_csv('Supplement/INH_Ci_5.csv')
+#inh_Ci_5.to_csv('Supplement/INH_Ci_5.csv')
 
 rif_Ci = gen_Ci_table(rif_gene_list, rif_file, prefix = 'D1_')
-rif_Ci.to_csv('Supplement/Rif_Ci.csv')
+#rif_Ci.to_csv('Supplement/Rif_Ci.csv')
 rif_Ci_5 = gen_Ci_table(rif_gene_list, rif_file5, prefix = 'D5_')
-rif_Ci_5.to_csv('Supplement/Rif_Ci_5.csv')
+#rif_Ci_5.to_csv('Supplement/Rif_Ci_5.csv')
 
 vanc_Ci = gen_Ci_table(vanc_gene_list, vanc_file, prefix = 'D1_')
 vanc_Ci.to_csv('Supplement/Vanc_Ci.csv')
@@ -208,14 +206,17 @@ vanc_Ci_5.to_csv('Supplement/Vanc_Ci_5.csv')
 
 
 
-output_path = '/Users/zacharyazadian/RockLab Dropbox/Zachary Azadian/ChemGen/New_Data_Results/AAA_Final_Data/Weizhen_Comp/Supplement/'
+output_path = 'Results/Supplemental_Data_2/'
 #### Have all 3 tables - Weizhen+Hit_Status, Mag_1 + Mag_5
 
 ###### Emb
-emb_weiz = pd.read_csv(output_path + 'Emb.csv', index_col=0)
+#emb_weiz = pd.read_csv(output_path + 'Emb.csv', index_col=0)
+emb_weiz = Emb_Tab
 emb_weiz = emb_weiz.rename(columns={'Full_ID':'Gene_ID'})
-emb_mag_1 = pd.read_csv(output_path + 'Emb_Ci.csv', index_col=0)
-emb_mag_5 = pd.read_csv(output_path + 'Emb_Ci_5.csv', index_col=0)
+#emb_mag_1 = pd.read_csv(output_path + 'Emb_Ci.csv', index_col=0)
+emb_mag_1 = emb_Ci
+#emb_mag_5 = pd.read_csv(output_path + 'Emb_Ci_5.csv', index_col=0)
+emb_mag_5 = emb_Ci_5
 emb_1 = pd.merge(emb_weiz, emb_mag_1, on='Gene_ID')
 emb_15 = pd.merge(emb_1, emb_mag_5, on='Gene_ID')
 emb_final = emb_15[['Gene_ID', 'Emb_l2fc', 'Emb_q', 'CRISPRi_Hit', 'D1_CRISPRi_L2FC', 'D1_CRISPRi_fdr', 'D5_CRISPRi_L2FC', 'D5_CRISPRi_fdr']]
@@ -223,10 +224,13 @@ emb_final.columns = ['ORF_ID', 'TnSeq_l2fc', 'TnSeq_p_adj', 'CRISPRi_Hit', 'CRIS
 emb_final = emb_final.set_index('ORF_ID')
 
 ####### inh
-inh_weiz = pd.read_csv(output_path + 'INH.csv', index_col=0)
+#inh_weiz = pd.read_csv(output_path + 'INH.csv', index_col=0)
+inh_weiz = Inh_Tab
 inh_weiz = inh_weiz.rename(columns={'Full_ID':'Gene_ID'})
-inh_mag_1 = pd.read_csv(output_path + 'INH_Ci.csv', index_col=0)
-inh_mag_5 = pd.read_csv(output_path + 'INH_Ci_5.csv', index_col=0)
+#inh_mag_1 = pd.read_csv(output_path + 'INH_Ci.csv', index_col=0)
+inh_mag_1 = inh_Ci
+#inh_mag_5 = pd.read_csv(output_path + 'INH_Ci_5.csv', index_col=0)
+inh_mag_5 = inh_Ci_5
 inh_1 = pd.merge(inh_weiz, inh_mag_1, on='Gene_ID')
 inh_15 = pd.merge(inh_1, inh_mag_5, on='Gene_ID')
 inh_final = inh_15[['Gene_ID', 'INH_l2fc', 'INH_q', 'CRISPRi_Hit', 'D1_CRISPRi_L2FC', 'D1_CRISPRi_fdr', 'D5_CRISPRi_L2FC', 'D5_CRISPRi_fdr']]
@@ -234,10 +238,13 @@ inh_final.columns = ['ORF_ID', 'TnSeq_l2fc', 'TnSeq_p_adj', 'CRISPRi_Hit', 'CRIS
 inh_final = inh_final.set_index('ORF_ID')
 
 ####### rif
-rif_weiz = pd.read_csv(output_path + 'Rif.csv', index_col=0)
+#rif_weiz = pd.read_csv(output_path + 'Rif.csv', index_col=0)
+rif_weiz = Rif_Tab
 rif_weiz = rif_weiz.rename(columns={'Full_ID':'Gene_ID'})
-rif_mag_1 = pd.read_csv(output_path + 'Rif_Ci.csv', index_col=0)
-rif_mag_5 = pd.read_csv(output_path + 'Rif_Ci_5.csv', index_col=0)
+#rif_mag_1 = pd.read_csv(output_path + 'Rif_Ci.csv', index_col=0)
+rif_mag_1 = rif_Ci
+#rif_mag_5 = pd.read_csv(output_path + 'Rif_Ci_5.csv', index_col=0)
+rif_mag_5 = rif_Ci_5
 rif_1 = pd.merge(rif_weiz, rif_mag_1, on='Gene_ID')
 rif_15 = pd.merge(rif_1, rif_mag_5, on='Gene_ID')
 rif_final = rif_15[['Gene_ID', 'Rif_l2fc', 'Rif_q', 'CRISPRi_Hit', 'D1_CRISPRi_L2FC', 'D1_CRISPRi_fdr', 'D5_CRISPRi_L2FC', 'D5_CRISPRi_fdr']]
@@ -245,17 +252,20 @@ rif_final.columns = ['ORF_ID', 'TnSeq_l2fc', 'TnSeq_p_adj', 'CRISPRi_Hit', 'CRIS
 rif_final = rif_final.set_index('ORF_ID')
 
 ####### vanc
-vanc_weiz = pd.read_csv(output_path + 'Vanc.csv', index_col=0)
+#vanc_weiz = pd.read_csv(output_path + 'Vanc.csv', index_col=0)
+vanc_weiz = Vanc_Tab
 vanc_weiz = vanc_weiz.rename(columns={'Full_ID':'Gene_ID'})
-vanc_mag_1 = pd.read_csv(output_path + 'Vanc_Ci.csv', index_col=0)
-vanc_mag_5 = pd.read_csv(output_path + 'Vanc_Ci_5.csv', index_col=0)
+#vanc_mag_1 = pd.read_csv(output_path + 'Vanc_Ci.csv', index_col=0)
+vanc_mag_1 = vanc_Ci
+#vanc_mag_5 = pd.read_csv(output_path + 'Vanc_Ci_5.csv', index_col=0)
+vanc_mag_5 = vanc_Ci_5
 vanc_1 = pd.merge(vanc_weiz, vanc_mag_1, on='Gene_ID')
 vanc_15 = pd.merge(vanc_1, vanc_mag_5, on='Gene_ID')
 vanc_final = vanc_15[['Gene_ID', 'Vanc_l2fc', 'Vanc_q', 'CRISPRi_Hit', 'D1_CRISPRi_L2FC', 'D1_CRISPRi_fdr', 'D5_CRISPRi_L2FC', 'D5_CRISPRi_fdr']]
 vanc_final.columns = ['ORF_ID', 'TnSeq_l2fc', 'TnSeq_p_adj', 'CRISPRi_Hit', 'CRISPRi_D1_l2fc', 'CRISPRi_D1_p_adj', 'CRISPRi_D5_l2fc', 'CRISPRi_D5_p_adj']
 vanc_final = vanc_final.set_index('ORF_ID')
 
-writer = pd.ExcelWriter(output_path+'Final/Supplemental_Data_2_weizhen_automated.xlsx')
+writer = pd.ExcelWriter(output_path+'Supplemental_Data_2.xlsx')
 emb_final.to_excel(writer, sheet_name='EMB')
 inh_final.to_excel(writer, sheet_name='INH')
 rif_final.to_excel(writer, sheet_name='RIF')
